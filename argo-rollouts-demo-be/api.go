518bb719c5eea7218baf16ee5f9cfa7615ce0787
Elad Hayun <elad.hayun@gmail.com>
2025-03-20 15:34:17 +0200

    Update build hash in API and sync git submodule for gitops repository to latest commit.

--- argo-rollouts-demo-be/api.go
+++ argo-rollouts-demo-be/api.go
@@ -22,7 +22,7 @@ var (
 	mu        sync.Mutex
 	version   = os.Getenv("VERSION") // Get version from environment variable
 	rng       = rand.New(rand.NewSource(time.Now().UnixNano()))
-	buildHash = "5vj790"
+	buildHash = "5vj791"
 )
 
 func checkHandler(c echo.Context) error {
