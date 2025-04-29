6dd223271e9f67a7cfd60b43d36e5658e9fbfcc3
Elad Hayun <elad.hayun@gmail.com>
2025-04-29 18:18:31 +0300

    Increment build hash in API from 5vj719 to 5vj720 for version consistency.

--- argo-rollouts-demo-be/api.go
+++ argo-rollouts-demo-be/api.go
@@ -22,7 +22,7 @@ var (
 	mu        sync.Mutex
 	version   = os.Getenv("VERSION") // Get version from environment variable
 	rng       = rand.New(rand.NewSource(time.Now().UnixNano()))
-	buildHash = "5vj719"
+	buildHash = "5vj720"
 )
 
 func checkHandler(c echo.Context) error {
