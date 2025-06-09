919b90cf5ba22722025e3e0415fe8b685178b56f
Elad Hayun <elad.hayun@gmail.com>
2025-06-09 19:05:21 +0300

    Update git submodule for gitops repository to latest commit ec92aa3, and increment build hash in API from 5vj734 to 5vj735 for version consistency.

--- argo-rollouts-demo-be/api.go
+++ argo-rollouts-demo-be/api.go
@@ -25,7 +25,7 @@ var (
 	mu        sync.Mutex
 	version   = os.Getenv("VERSION") // Get version from environment variable
 	rng       = rand.New(rand.NewSource(time.Now().UnixNano()))
-	buildHash = "5vj734"
+	buildHash = "5vj735"
 
 	// Prometheus metrics
 	httpRequestsTotal = promauto.NewCounterVec(
