060f0fe2ae30dc4d8381e8661b9cdc6ddade23f7
Elad Hayun <elad.hayun@gmail.com>
2025-06-09 18:20:20 +0300

    Increment build hash in API from 5vj727 to 5vj728 for version consistency.

--- argo-rollouts-demo-be/api.go
+++ argo-rollouts-demo-be/api.go
@@ -25,7 +25,7 @@ var (
 	mu        sync.Mutex
 	version   = os.Getenv("VERSION") // Get version from environment variable
 	rng       = rand.New(rand.NewSource(time.Now().UnixNano()))
-	buildHash = "5vj727"
+	buildHash = "5vj728"
 
 	// Prometheus metrics
 	httpRequestsTotal = promauto.NewCounterVec(
